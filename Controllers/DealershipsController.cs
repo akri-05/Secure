7340466363c787c00b63f50b30862da4d7b75e1a
akri-05 <44156000+akri-05@users.noreply.github.com>
2018-10-15 13:25:29 -0400

    Add files via upload

---
+++ Controllers/DealershipsController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Lab1B.Data;
+using Lab1B.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Lab1B.Controllers
+{
+    [Authorize(Roles = "Employee, Manager, Admin")]
+    public class DealershipsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DealershipsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Dealerships
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Dealership.ToListAsync());
+        }
+
+        // GET: Dealerships/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dealership = await _context.Dealership
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (dealership == null)
+            {
+                return NotFound();
+            }
+
+            return View(dealership);
+        }
+
+        // GET: Dealerships/Create
+        [Authorize(Roles = "Manager, Admin")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dealerships/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Email,PhoneNumer")] Dealership dealership)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(dealership);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dealership);
+        }
+
+        // GET: Dealerships/Edit/5
+        [Authorize(Roles = "Manager, Admin")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dealership = await _context.Dealership.SingleOrDefaultAsync(m => m.ID == id);
+            if (dealership == null)
+            {
+                return NotFound();
+            }
+            return View(dealership);
+        }
+
+        [Authorize(Roles = "Manager, Admin")]
+        // POST: Dealerships/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Email,PhoneNumer")] Dealership dealership)
+        {
+            if (id != dealership.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(dealership);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DealershipExists(dealership.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(dealership);
+        }
+        [Authorize(Roles = "Manager, Admin")]
+        // GET: Dealerships/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var dealership = await _context.Dealership
+                .SingleOrDefaultAsync(m => m.ID == id);
+            if (dealership == null)
+            {
+                return NotFound();
+            }
+
+            return View(dealership);
+        }
+        [Authorize(Roles = "Manager, Admin")]
+        // POST: Dealerships/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var dealership = await _context.Dealership.SingleOrDefaultAsync(m => m.ID == id);
+            _context.Dealership.Remove(dealership);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DealershipExists(int id)
+        {
+            return _context.Dealership.Any(e => e.ID == id);
+        }
+    }
+}
